4ee02f316c5a1d04288252e49abf18336d100075
Chandana pavithran <chandanapavithran120@gmail.com>
2026-02-22 00:33:28 +0530

    Fix unresolved reference: rename google-material alias to googleMaterial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.google.material)
+    implementation(libs.googleMaterial)
     
     // Room
     implementation(libs.androidx.room.runtime)
